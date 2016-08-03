2ea799175ab2532fc26a196a9eedc4a3259fe3ca
Candy Gumdrop <candy@candygumdrop.com>
2016-08-03 15:13:12 +0100

    Add Hex info to mix.exs

--- mix.exs
+++ mix.exs
@@ -14,10 +14,28 @@ defmodule Proj.Mixfile do
      compilers: [:proj, :elixir, :app],
      deps: deps,
      description: "Elixir coordinate conversion library using OSGeo's PROJ.4",
-     name: "proj"]
+     name: "proj",
+     package: package]
   end
 
   def application, do: []
+
+  defp package do
+    [maintainers: ["Candy Gumdrop"],
+     licenses: ["CC0"],
+     links: %{"GitHub" => "https://github.com/candygumdrop/proj",
+              "Issues" => "https://github.com/candygumdrop/proj/issues"},
+     files: ["src",
+             "priv/.gitignore",
+             "config",
+             "lib",
+             "mix.exs",
+             "Makefile",
+             "test",
+             "COPYING",
+             "README.md"]
+    ]
+  end
 
   defp deps do
     []
