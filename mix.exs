cb40380ecd1e4ed3cf32d05de35d8b48b8083742
Candy Gumdrop <candy@candygumdrop.com>
2016-08-03 15:23:15 +0100

    Add ex_doc dependency

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Proj.Mixfile do
 
   def project do
     [app: :proj,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.0",
      compilers: [:proj, :elixir, :app],
      deps: deps,
@@ -38,6 +38,6 @@ defmodule Proj.Mixfile do
   end
 
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
   end
 end
