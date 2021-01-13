c8cd19461ab39369825b77f64209bb4f4bde96b3
Candy Gumdrop <candy@candygumdrop.com>
2021-01-13 14:51:01 +0000

    Use explicit EPSG:27700 PROJ def string in tests
    This fixes broken transformations where newer versions of PROJ may have
    missing OSGB 1936 -> WGS 84 datum shift data.

--- test/proj_test.exs
+++ test/proj_test.exs
@@ -2,7 +2,7 @@ defmodule ProjTest do
   use ExUnit.Case
 
   @wgs84_def "+init=epsg:4326"
-  @epsg_27700_def "+init=epsg:27700"
+  @epsg_27700_def "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs"
 
   @deg_rad 0.0174532925
   @rad_deg 1 / @deg_rad
